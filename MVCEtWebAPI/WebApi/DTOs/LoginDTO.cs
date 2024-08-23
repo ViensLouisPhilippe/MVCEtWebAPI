796fe023ab624cd27caeb620d940549edece58bc
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2024-08-23 16:48:31 -0400

    Cours 1 Initiation projet solution

---
+++ MVCEtWebAPI/WebApi/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; } = "";
+        [Required]
+        public string Password { get; set; } = "";
+    }
+
+   
+}
---
+++ MVCEtWebAPI/WebApi/DTOs/LoginSuccess.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DTOs
+{
+    public class LoginSuccessDTO
+    {
+        [Required]
+        public string Token { get; set; } = "";
+
+    }
+
+}
